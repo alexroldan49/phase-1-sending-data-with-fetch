18768651fcc8f77515bc5bef3ed4d971f1bed7d3
Alexander Roldan <alexroldan49@gmail.com>
2021-07-30 10:47:11 -0400

    commit tree 1cd7fe2d56574b8999510e336a79e194b5e6be43

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Add your code here
+const nameEmail = {
+    name: "sarah",
+    email: "SarahColgate@yahoo.com"
+}
+const bodyE = documemnt.querySelector("body")
+// create  submitData function
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({name,email})
+
+    })
+    .then(resp => {return resp.json()})
+    .then( resp => document.querySelector("body").append(resp.id))
+    .catch(error => document.body.append(error.message))
+    
+}
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   };
+  
+//   // method: "POST" is missing from the object below
+//   const configurationObject = {
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(object) {
+//       console.log(object);
+//     })
+//     .catch(function(error) {
+//       alert("Bad things! Ragnarők!");
+//       console.log(error.message);
+//     }); 
